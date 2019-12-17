9c344e3152f8464d616e0420d6849895d5913061
Brent Lineberry <brent.lineberry@emory.edu>
2019-12-17 09:58:45 -0500

    refactored eddington calculations

--- actions/home.js
+++ actions/home.js
@@ -3,79 +3,7 @@ const refreshStravaToken = require('../middleware/refreshStravaToken');
 const primeStravaApi = require('../middleware/primeStravaApi');
 const loadAthlete = require('../middleware/loadAthlete');
 const fetchNewStravaActivities = require('../middleware/fetchNewStravaActivities');
-
-function calculateEddingtonNumbers(activities) {
-    let counts = {};
-    let activityDays = [];
-    let dayIndex = {};
-
-    for (let i = 0; i < activities.length; i++) {
-        let activity = activities[i];
-        let day = activity.startDate.toDateString();
-        let indexKey = activity.type + day;
-        let index = dayIndex[indexKey];
-
-        if (!index)
-        {
-            index = activityDays.length;
-            dayIndex[indexKey] = index;
-            activityDays[index] = {
-                type: activity.type,
-                distance: 0,
-                day: day
-            };
-        }
-
-        activityDays[index].distance += activity.distance;
-    }
-
-    for (let i = 0; i < activityDays.length; i++) {
-        let activityDay = activityDays[i];
-        let type = activityDay.type;
-
-        if (!counts[type]) {
-            counts[type] = {
-                eNum: 0
-            };
-        }
-
-        let miles = Math.floor(activityDay.distance / 1609.344);
-
-        if (miles < counts[type].eNum) {
-            continue;
-        }        
-
-        for (let j = miles; j > counts[type].eNum; j--) {
-            if (!counts[type][j]) {
-                counts[type][j] = 0;
-            }
-    
-            counts[type][j] += 1;
-
-            if (type === "Run" && j === 12) {
-                console.log(activityDay.day);
-            }
-
-            if (counts[type][j] >= j) {
-                counts[type].eNum = j;
-                break;
-            }
-        }
-    }
-
-    let countsAsArray = [];
-    for(var key in counts) {
-        if(!counts.hasOwnProperty(key)) {
-            continue;
-        }
-
-        countsAsArray.push({
-            type: key,
-            eNum: counts[key].eNum
-        });
-    }
-    return countsAsArray;
-};
+const eddington = require('../eddington');
 
 let action = (req, res) => {
     req.appData.db.activity.findAll({
@@ -84,10 +12,26 @@ let action = (req, res) => {
         }
     })
     .then(activities => {
-        let eddingtonNumbers = calculateEddingtonNumbers(activities);
+        let activityTypes = activities.map(a => a.type).filter((v,i,a) => {
+            return i === a.indexOf(v);
+        });
+        let eddingtonNumbers = [];
+
+        for (let i = 0; i < activityTypes.length; i++) {
+            let type = activityTypes[i];
+            eddingtonNumbers.push({
+                type: type,
+                eNum: eddington.getENumFromActivities(activities.filter(a => a.type === type).map(a => {
+                    return {
+                        distance: a.distance / 1609.344,
+                        date: a.startDate
+                    };
+                })),
+            });
+        }
 
         res.render('home.njk', {
-            athlete: req.appData.athlete, 
+            athlete: req.appData.athlete,
             eddingtonNumbers,
             activityCount: activities.length,
         });
