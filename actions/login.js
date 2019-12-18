9c675fa331059d953d950eb9b30ebbbd396e0fa2
Brent Lineberry <brent@thelineberrys.com>
2019-12-17 22:30:17 -0500

    fixed hardcoded strava auth redirect_uri, added activity:read_all scope to get private activities too

--- actions/login.js
+++ actions/login.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    let stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${ process.env.STRAVA_CLIENT_ID }&redirect_uri=${ encodeURIComponent("http://localhost:3000/auth") }&response_type=code&scope=read,activity:read`;
+    let stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${ process.env.STRAVA_CLIENT_ID }&redirect_uri=${ encodeURIComponent( process.env.STRAVA_REDIRECT_DOMAIN + "/auth") }&response_type=code&scope=read,activity:read,activity:read_all`;
     res.render('login.njk', { stravaAuthUrl });
 };
