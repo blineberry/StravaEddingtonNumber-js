89257ac43f58cd2b51bfefc965245c668de9c8c1
Brent Lineberry <brent.lineberry@emory.edu>
2019-12-19 10:52:46 -0500

    added dbconfig for dev db, added support to force HTTP on heroku

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ const dbConfig = require('./dbconfig.js');
 const app = express();
 const port = process.env.PORT;
 
+// setup view engine
 nunjucks.configure('views', {
     express: app
 });
 
-const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.pass, dbConfig.options);
+// Force https if on Heroku
+app.use((req, res, next) => {
+    if (process.env.HEROKU_SSL !== "force") {
+        return next();
+    }
 
+    let proto = req.get('X-Forwarded-Proto');
+
+    if (proto === 'https') {
+        return next();
+    }
+
+    if (req.method === 'GET') {
+        return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
+    }
+
+    return res.status(403).send('HTTPS Required');
+});
+
+// setup session
 app.use(session({
     secret: process.env.SESSION_SECRET.split(','),
     secure: process.env.NODE_ENV === "development" ? false : true,
@@ -24,20 +43,35 @@ app.use(session({
     saveUninitialized: false
 }));
 
+let dbInitialized = false;
+
+// initialize db
+const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.pass, dbConfig.options);
 data.init(sequelize);
-
 sequelize.authenticate()
-    .then(() => {
-        console.log('Connected');        
-    })
-    .catch(err => {
-        console.log(err);
-    });
+.then(() => {
+    console.log('Connected');     
+    dbInitialized = true;   
+})
+.catch(err => {
+    console.log(err);
+});
 
+// send a sensible response if db connection failed
+app.use((req,res,next) => {
+    if (dbInitialized) {
+        next();
+        return;
+    }
+
+    res.status(503).send("Unable to connect to database");
+});
 
 if (process.env.NODE_ENV === "development") {
     sequelize.sync({ force: false });
 }
+
+// get the db into the pipeline
 app.use((req, res, next) => {
     if (!req.appData) {
         req.appData = {};
@@ -47,13 +81,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// serve static files
 app.use('/static', express.static('public'));
 
 app.get('/', actions.home);
 app.get('/login', actions.login);
 app.get('/auth', actions.auth);
 app.get('/eddington', actions.eddington);
-
 app.post('/deauthorize', actions.deauthorize);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,31 @@
 const Sequelize = require('sequelize');
 
-module.exports = {
+dev = {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    options: {
+        dialect: 'mssql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        encrypt: true,
+        dialectOptions: { 
+        options: {
+            encrypt: true
+        }
+        },
+        retry: {
+            match: [
+                Sequelize.TimeoutError,
+                Sequelize.ConnectionError,
+                Sequelize.ConnectionTimedOutError
+            ],
+            max: 3
+        }
+    }
+}
+
+prod = {
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
     pass: process.env.DB_PASS,
@@ -22,3 +47,5 @@ module.exports = {
         }
     }
 };
+
+module.exports = process.env.NODE_ENV === 'development' ? dev : prod;
