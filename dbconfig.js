faa0eb8c12941c946046fb22cf498d69c127ddc5
Brent Lineberry <brent@thelineberrys.com>
2019-12-18 22:37:54 -0500

    dangit still need db config in env variables

--- dbconfig.js
+++ dbconfig.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize');
 
 module.exports = {
-    name: '',
-    user: '',
-    pass: '',
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
     options: {
         dialect: 'postgres',
         protocol: 'postgres',
-        host: '',
+        host: process.env.DB_HOST,
         dialectOptions: {
             ssl: true
         },
-        port: ,
+        port: process.env.DB_PORT,
         retry: {
             match: [
                 Sequelize.TimeoutError,
