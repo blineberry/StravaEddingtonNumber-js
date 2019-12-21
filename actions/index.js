d3a5b49c39f7fb107f796bd217461ca6e873d439
Brent Lineberry <brent@thelineberrys.com>
2019-12-20 21:35:33 -0500

    clean up after account refactor

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,4 @@
 module.exports = {
     home: require('./home'),
-    auth: require('./auth'),
-    login: require('./login'),
-    deauthorize: require('./deauthorize'),
     eddington: require('./eddington'),
-    logout: require('./logout'),
-    account: require('./account'),
 };
--- actions/logout.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            console.log(err);
-            res.send('error');
-        }
-        res.redirect('/');
-    });
-}
